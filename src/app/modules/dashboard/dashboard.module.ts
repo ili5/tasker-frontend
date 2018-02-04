084b2c8668a7a1f7325b29a5cc1a5b0be9bdc285
Ilija Petkovic <ilija.petkovic93@gmail.com>
2018-02-04 17:12:17 +0100

    update

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from "@angular/core";
 import {DashboardRoutingModule} from "./dashboard-routing.module";
-import {ProjectComponent} from "./projects/project.component";
 import {DashboardComponent} from "./dashboard.component";
-import {TaskComponent} from "./task/task.component";
 
 @NgModule({
   imports: [
